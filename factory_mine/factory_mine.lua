2b5c3834dbb42575052a978ff778aa85dc23e963
JoseCodeBR <joseanastaciodiascastilho@gmail.com>
2026-08-30 21:43:08 -0300

    Update factory_mine.lua

--- factory_mine/factory_mine.lua
+++ factory_mine/factory_mine.lua
@@ -1,146 +1,161 @@
-local repor_node="false"
-local item1 = "searchandrescue:heli"
-local produto_saida = "searchandrescue:heli"
-local furnace_inactive_formspec ="formspec_version[5] size[14,10]"..
-	"label[5,0.6;use:"..item1.."]"..	
-	"label[5.5,2.1;input]"..
-	"list[current_name;item1;5.4,2.6;1,1;0]"..
-	"list[current_player;main;2.2,8.3;8,1;1]"..
-	"list[current_player;main;2.2,4.3;8,3;0]"
-	
+-- ========================================================================
+-- FACTORY MINE - MÓDULO PRINCIPAL
+-- ========================================================================
+local item_requisitado = "searchandrescue:heli"
+local entidade_gerada  = "searchandrescue:helicopter"
+
+local furnace_formspec = 
+    "formspec_version[5]" ..
+    "size[14,10]" ..
+    "label[5,0.6;Insira: " .. item_requisitado .. "]" ..
+    "label[5.5,2.1;Entrada]" ..
+    "list[current_name;item1;5.4,2.6;1,1;0]" ..
+    "list[current_player;main;2.2,8.3;8,1;1]" ..
+    "list[current_player;main;2.2,4.3;8,3;0]"
+
+-- Declaração antecipada de funções auxiliares
+local add_acessorios
+local limpar_area
+
+-- ========================================================================
+-- REGISTRO DO BLOCO DA FÁBRICA
+-- ========================================================================
 minetest.register_node("factory_mine:factory", {
-	description = "factory",
-	visual_size = {x = 1,y = 1},
+    description = "Fábrica de Montagem",
+    visual_size = {x = 1, y = 1},
 
-	selection_box = {type = "fixed",	fixed = {-4,-5,-4, 4,0,4},},
-	collision_box = {type = "fixed",	fixed = {-4,-5,-4, 4,0,4},},
+    selection_box = {type = "fixed", fixed = {-4, -5, -4, 4, 0, 4}},
+    collision_box = {type = "fixed", fixed = {-4, -5, -4, 4, 0, 4}},
 
-	drawtype = "mesh",
-	mesh = "factory_mine.b3d",
-    tiles = {"factory_mine.png"},
-	inventory_image="factory_mine_icon.png",
-	wield_image="factory_mine_icon.png",
-	legacy_facedir_simple = true,
-	paramtype = "light",
-	--paramtype2 = "facedir",
-	groups = {	snappy=1, choppy=2, flammable=3, oddly_breakable_by_hand=2, not_in_creative_inventory=0},
-
-on_construct = function(pos)
-		local meta = minetest.env:get_meta(pos)
-		meta:set_string("formspec", furnace_inactive_formspec)
-		meta:set_string("infotext", "factory_mine")
-		local inv = meta:get_inventory()
-		inv:set_size("item1", 1)
-		add_acessorios(pos,player)
-end,
-
-can_dig = function(pos,player)
-		local meta = minetest.env:get_meta(pos);
-		local inv = meta:get_inventory()
-		if not inv:is_empty("item1") then return false end
-		limpar_area(pos,player)
-return true end,
-})
-
-minetest.register_node("factory_mine:factory_active", {
-	description = "factory_active",
-	visual_size = {x = 1,y = 1},
     drawtype = "mesh",
-	mesh = "factory_mine.b3d",
+    mesh = "factory_mine.b3d",
     tiles = {"factory_mine.png"},
-	paramtype = "light",
-	light_source = 13,
-	drop = "factory_mine:factory",
-	groups = {	snappy=1, choppy=2, flammable=3, oddly_breakable_by_hand=2, not_in_creative_inventory=1},
+    inventory_image = "factory_mine_icon.png",
+    wield_image = "factory_mine_icon.png",
+    paramtype = "light",
+    groups = {snappy = 1, choppy = 2, flammable = 3, oddly_breakable_by_hand = 2},
 
-on_construct = function(pos)
-		local meta = minetest.env:get_meta(pos)
-		meta:set_string("formspec", furnace_inactive_formspec)
-		local inv = meta:get_inventory()
-		inv:set_size("item1", 1)
-	end,
+    on_construct = function(pos)
+        local meta = minetest.get_meta(pos)
+        meta:set_string("formspec", furnace_formspec)
+        meta:set_string("infotext", "Fábrica de Veículos")
+        meta:set_int("Total", 0)
+        
+        local inv = meta:get_inventory()
+        inv:set_size("item1", 1)
 
-can_dig = function(pos,player)
-		local meta = minetest.env:get_meta(pos);
-		local inv = meta:get_inventory()
-		if not inv:is_empty("item1") then return false end
-		limpar_area(pos,player)
-return true end,
+        -- Adiciona as luzes periféricas e o robô operário
+        add_acessorios(pos)
+    end,
+
+    -- Dispara quando o jogador insere um item na interface
+    on_metadata_inventory_put = function(pos, listname, index, stack, player)
+        if listname == "item1" and stack:get_name() == item_requisitado then
+            local timer = minetest.get_node_timer(pos)
+            if not timer:is_started() then
+                timer:start(1.0) -- Inicia o processamento
+            end
+        end
+    end,
+
+    -- Timer de processamento (substitui o ABM de alta carga)
+    on_timer = function(pos, elapsed)
+        local meta = minetest.get_meta(pos)
+        local inv = meta:get_inventory()
+        local stack = inv:get_stack("item1", 1)
+
+        if stack:get_name() == item_requisitado then
+            -- Consome 1 item do slot
+            stack:take_item(1)
+            inv:set_stack("item1", 1, stack)
+
+            -- Spawn do helicóptero sobre a fábrica
+            local spawn_pos = vector.add(pos, {x = 0, y = 1, z = 0})
+            if minetest.get_node(spawn_pos).name == "air" then
+                minetest.add_entity(spawn_pos, entidade_gerada)
+            end
+
+            -- Atualiza o contador de produção
+            local total = meta:get_int("Total") + 1
+            meta:set_int("Total", total)
+            meta:set_string("formspec", furnace_formspec .. "label[4.5,1.5;Total Produzido: " .. total .. "]")
+
+            -- Se ainda houverem itens no slot, agenda o próximo ciclo
+            if not inv:get_stack("item1", 1):is_empty() then
+                return true
+            end
+        end
+        return false
+    end,
+
+    can_dig = function(pos, player)
+        local meta = minetest.get_meta(pos)
+        local inv = meta:get_inventory()
+        
+        if not inv:is_empty("item1") then 
+            return false 
+        end
+        
+        limpar_area(pos)
+        return true
+    end,
+
+    on_destruct = function(pos)
+        limpar_area(pos)
+    end,
 })
 
-function hacky_swap_node(pos,name)
-	local node = minetest.env:get_node(pos)
-	local meta = minetest.env:get_meta(pos)
-	local meta0 = meta:to_table()
-	if node.name == name then return end
-	node.name = name
-	local meta0 = meta:to_table()
-	minetest.env:set_node(pos,node)
-	meta = minetest.env:get_meta(pos)
-	meta:from_table(meta0)
+-- ========================================================================
+-- FUNÇÕES AUXILIARES DE ESTRUTURA E LIMPEZA
+-- ========================================================================
+
+add_acessorios = function(pos)
+    local posicoes_luz = {
+        {5, 3, 3},
+        {5, 3, -3},
+        {-5, 3, -3},
+        {-5, 3, 3},
+    }
+
+    -- Instancia o Robô Operário no centro se o nó estiver livre
+    local node_center = minetest.get_node(pos)
+    if node_center.name ~= "ignore" then
+        minetest.add_entity(pos, "factory_mine:robo_operario")
+    end
+
+    -- Posiciona as luminárias industriais
+    for _, offset in ipairs(posicoes_luz) do
+        local light_pos = vector.add(pos, {x = offset[1], y = offset[2], z = offset[3]})
+        if minetest.get_node(light_pos).name == "air" then
+            minetest.set_node(light_pos, {name = "factory_mine:factory_mine_light"})
+        end
+    end
 end
 
-minetest.register_abm({
-	nodenames = {"factory_mine:factory","factory_mine:factory_active"},
-	interval =1,
-	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		local meta = minetest.env:get_meta(pos)
-		local inv = meta:get_inventory()
-		local stack = inv:get_stack("item1",1)
-		if stack:get_name() == item1 then
-			inv:remove_item("item1",ItemStack(item1))
+limpar_area = function(pos)
+    local posicoes_luz = {
+        {5, 3, 3},
+        {5, 3, -3},
+        {-5, 3, -3},
+        {-5, 3, 3},
+    }
 
-			local position = vector.add({x=0, y=1, z=0},pos)
-			if  minetest.get_node(position).name == "air" then
-				local node_add=minetest.add_entity(position, "searchandrescue:helicopter")
-			end
+    -- Remove as luzes criadas no perímetro
+    for _, offset in ipairs(posicoes_luz) do
+        local light_pos = vector.add(pos, {x = offset[1], y = offset[2], z = offset[3]})
+        if minetest.get_node(light_pos).name == "factory_mine:factory_mine_light" then
+            minetest.set_node(light_pos, {name = "air"})
+        end
+    end
 
-			meta:set_int("Total",meta:get_int("Total")+1)
-			meta:set_string("formspec", furnace_inactive_formspec.."label[4.5,1.5;Total: "..meta:get_int("Total"))
-		end
-	end
-})
-
-function add_acessorios(pos, player)
-local posicoes={
-				{5,3,3}, --1
-				{5,3,-3},--2
-				{-5,3,-3},--3
-				{-5,3,3},--4
-	}
-	posicao = vector.add(pos,{x=0, y=0, z=0})
-	if  minetest.get_node(posicao).name == "air" then
-		local robot=minetest.add_entity(posicao, "factory_mine:robo_operario")
-	end
-
-		for i = 1, #posicoes, 1 do  
-			local position = vector.add({x=posicoes[i][1], y=posicoes[i][2], z=posicoes[i][3]},pos)
-			if  minetest.get_node(position).name == "air" then
-			local node_add=minetest.set_node(position, {name="factory_mine:factory_mine_light"})
-		end
-	end end
-
-function limpar_area(pos, player)
---	local posicao = minetest.env:get_node(pos)
-	for _, ob in ipairs(minetest.get_objects_inside_radius(pos,50)) do
-		local luaentity = ob:get_luaentity()
-		if not ob:is_player() and
-					luaentity and
-					luaentity.itemstring ~="" and
-					luaentity.name=="factory_mine:robo_operario" then
-			ob:remove() 
-local posicoes={
-				{5,3,3}, --1
-				{5,3,-3},--2
-				{-5,3,-3},--3
-				{-5,3,3},--4
-	}
-		for i = 1, #posicoes, 1 do  
-			local position = vector.add({x=posicoes[i][1], y=posicoes[i][2], z=posicoes[i][3]},pos)
-		if  minetest.get_node(position).name == "factory_mine:factory_mine_light" then
-			minetest.chat_send_all("removido")
-			local node_add=minetest.set_node(position, {name="air"})
-		end
-	end end
-end end
+    -- Remove a entidade do Robô Operário associada
+    local objects = minetest.get_objects_inside_radius(pos, 15)
+    for _, obj in ipairs(objects) do
+        if not obj:is_player() then
+            local luaentity = obj:get_luaentity()
+            if luaentity and luaentity.name == "factory_mine:robo_operario" then
+                obj:remove()
+            end
+        end
+    end
+end
