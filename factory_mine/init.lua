e474a6c3c370b4b3cd8c95ef70d1579c06844d4f
JoseCodeBR <joseanastaciodiascastilho@gmail.com>
2026-08-30 22:05:13 -0300

    Fix loading of 'acessorios.lua' in initialization

--- factory_mine/init.lua
+++ factory_mine/init.lua
@@ -1,6 +1,7 @@
 -- ========================================================================
 -- FACTORY MINE - INITIALIZATION
 -- ========================================================================
+
 -- Tabela global para exportar funções e APIs do mod
 factory_mine = factory_mine or {}
 
@@ -10,7 +11,4 @@ local modpath = minetest.get_modpath("factory_mine")
 -- Carregamento dos sub-módulos do mod
 dofile(modpath .. "/factory_mine.lua")
 dofile(modpath .. "/robo_operario.lua")
-dofile(modpath .. "/acessorios.lua")factory_mine = {}
-dofile(minetest.get_modpath("factory_mine") .. DIR_DELIM .. "factory_mine.lua")
-dofile(minetest.get_modpath("factory_mine") .. DIR_DELIM .. "robo_operario.lua")
-dofile(minetest.get_modpath("factory_mine") .. DIR_DELIM .. "acessorios.lua")
+dofile(modpath .. "/acessorios.lua")
